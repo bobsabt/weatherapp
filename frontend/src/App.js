bbbdebde98455b8f0ca7d7fd95d2fd0adc273c4b
bobsabt <bodai.barbara@gmail.com>
2022-02-07 17:10:32 +0100

    get Budapest weather data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,24 @@ import Navbar from './Components/Navbar';
 import './index.css';
 
 function App() {
+  
+  // Get the api key
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  
+
+  // By default it starts with Budapest weather 
+  React.useEffect(() =>
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=Budapest&days=3`)
+    .then((response) => response.json())
+    .then((d) => {
+      console.log(d)
+    })
+    .catch((err) => {
+      console.log("Error:" + err)
+    }),
+    []
+  );
+
   return (
     <div className="App">
       <Navbar />
